b48b1a4146b421074a6307ed97ec76447544dc9d
Nikolas <nikolas.silva1990@gmail.com>
2021-08-23 16:34:14 -0300

    Code Climate issues solved

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,1 @@
--- 3 - Escreva uma query que exiba os valores da coluna que contém a primary key da tabela `products`.
-
 SELECT id FROM northwind.products;
--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,1 @@
--- 4 - Conte quantos registros existem na coluna `product_name` da tabela `products`.
-
 SELECT COUNT(product_name) FROM northwind.products;
