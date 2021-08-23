6d193ffb32cff1dd3cb300ac5f3cb973c13eaa36
Filizzola <juliofilizzola@hotmail.com>
2021-08-23 16:53:01 -0300

    ref desafio 24

--- desafio24.sql
+++ desafio24.sql
@@ -1,3 +1,1 @@
-UPDATE northwind.order_details
-SET discount = 30
-WHERE UNIT_PRICE < 10.0000 AND id BETWEEN 30 AND 40 ;
+UPDATE northwind.order_details SET discount = 30 WHERE UNIT_PRICE < 10.0000 AND id BETWEEN 30 AND 40 ;
