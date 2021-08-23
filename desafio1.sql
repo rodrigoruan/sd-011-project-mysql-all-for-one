6b89a900903fb78639e36cd105ca0749bc77309e
Nikolas <nikolas.silva1990@gmail.com>
2021-08-23 16:27:50 -0300

    Desafios 1 - 8 concluídos

---
+++ desafio1.sql
@@ -0,0 +1,3 @@
+-- 1 - Exiba apenas os nomes dos produtos da tabela `products`.
+
+SELECT product_name FROM northwind.products;
---
+++ desafio3.sql
@@ -0,0 +1,3 @@
+-- 3 - Escreva uma query que exiba os valores da coluna que contém a primary key da tabela `products`.
+
+SELECT id FROM northwind.products;
