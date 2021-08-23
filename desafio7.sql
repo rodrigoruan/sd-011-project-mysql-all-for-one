73e993ee31562df025380fa8f6dd2b7f163e8271
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-08-23 17:24:01 -0300

    Update desafio7.sql

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 SELECT id FROM northwind.products
 ORDER BY id DESC
 LIMIT 5;
-=======
-SELECT id FROM northwind.products ORDER BY id DESC LIMIT 45 OFFSET 40;
->>>>>>> a419cf19ec2c0b2ffee2f477cbcb0e2069e505ab
