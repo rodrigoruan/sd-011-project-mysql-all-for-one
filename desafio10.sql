c6ee99ec4dbf1d4a985576d4199032953fb7b00f
marcoswarmling <marcoswarmling15@gmail.com>
2021-08-23 17:06:50 -0300

    desafios sobre filtragem de dados terminado

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,3 @@
-instala
+SELECT * FROM purchase_orders
+WHERE created_by > 3
+ORDER BY created_by DESC, id ASC;
---
+++ desafio19.sql
@@ -0,0 +1,2 @@
+SELECT * FROM orders
+WHERE employee_id = 5 || employee_id = 6 && shipper_id = 2;
