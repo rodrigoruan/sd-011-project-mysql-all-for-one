24e927745343e7472d83ac755011e559f6038373
Jessica Arruda <sicarruda@gmail.com>
2021-08-23 17:11:56 -0300

    exercicios 10 e 13 concludos

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,2 @@
---  em ordem decrescente ordenados por created_by . E como critério de desempate para a ordenação, ordene também os resultados pelo id de forma crescente.
 SELECT * FROM northwind.purchase_orders
-WHERE created_by > 3 ORDER BY created_by DESC, id; 
+WHERE created_by >= 3 ORDER BY created_by DESC, id; 
