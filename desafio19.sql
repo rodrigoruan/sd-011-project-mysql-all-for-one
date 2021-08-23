9115bb2d02e5af665555ab5153facd43b2509932
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-08-23 16:21:15 -0300

    Organizando as linhas

--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,7 @@
-SELECT COUNT(employee_id) AS orders_count FROM northwind.orders
-WHERE employee_id = 5 OR employee_id = 6
+SELECT COUNT(employee_id) 
+AS orders_count 
+FROM northwind.orders
+WHERE employee_id = 5 
+OR employee_id = 6
 AND shipper_id = 2;
+
