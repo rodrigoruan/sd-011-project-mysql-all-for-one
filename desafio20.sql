cf42d6f1f6b730d459b11ef1d12af950d7b8c98d
Marco Galindo <marco1408@gmail.com>
2021-08-23 17:27:58 -0300

    desafio 20 finalizado

---
+++ desafio20.sql
@@ -0,0 +1,2 @@
+INSERT INTO order_details (id, order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUES (92, 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
