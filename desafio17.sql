db4e17a18b2cc70ee7040e62f5b423b592ea677e
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-08-23 16:42:17 -0300

    Refatora tudo para uma linha

--- desafio17.sql
+++ desafio17.sql
@@ -1,3 +1,1 @@
-SELECT supplier_id 
-FROM northwind.purchase_orders
-WHERE (supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7);
+SELECT supplier_id FROM northwind.purchase_orders WHERE (supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7);
